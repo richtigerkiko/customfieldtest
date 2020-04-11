ca48ccbb0b1d96f86b5a70e819cb708ef16ec7a3
Alexander Kiko <elkikos@gmail.com>
2020-04-11 14:41:18 +0200

    addded deliverytime

--- are_you_eighteen.php
+++ are_you_eighteen.php
@@ -2,8 +2,12 @@
 
 /**
  * Add this code to functions.php of used theme (without <? ?>)
- * Add are you 18 field to checkout
  */
+
+
+ /**
+  * Are you 18? checkbox
+  */
 
 add_action( 'woocommerce_after_order_notes', 'are_you_eighteen_field' );
 
@@ -16,9 +20,32 @@ function are_you_eighteen_field( $checkout ) {
         'class'         => array('my-field-class form-row-wide'),
         'label'         => __('Hiermit bestätige ich, dass ich 18 bin'),
         ), $checkout->get_value( 'are_you_eighteen' ));
-
+ <
     echo '</div>';
+}
 
+
+/**
+ * Delivery Time Radiobuttons
+ */
+
+add_action ('woocommerce_after_order_notes', 'delivery_time_field');
+
+function delivery_time_field( $checkout ){
+    echo '<div id="delivery_time_field"<h2>' . __('Lieferzeit') . '</h2>';
+
+    woocommerce_form_field( 'delivery_time', array(
+        'type' => 'radio',
+        'class' => array('form-row-wide'),
+        'options' => array(
+            '18:00' => '18:00 - 19:00',
+            '19:00' => '19:00 - 20:00',
+            '20:00' => '20:00 - 21:00',
+            '21:00' => '21:00 - 22:00'
+        ),
+        'default' => '20:00'
+    ));
+    echo '</div>';
 }
 
 /**
@@ -26,4 +53,3 @@ function are_you_eighteen_field( $checkout ) {
  */
 
 ?>
-
